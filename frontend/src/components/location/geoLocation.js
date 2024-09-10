6ebd7022106ac34584061f6f09943a5afbb00483
Nand2004 <nandp480@gmail.com>
2024-09-10 12:25:04 -0400

    Creating the backend route for Location.

--- frontend/src/components/location/geoLocation.js
+++ frontend/src/components/location/geoLocation.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const GeolocationComponent = () => {
   const [location, setLocation] = useState({ latitude: null, longitude: null });
   const [error, setError] = useState(null);
 
+  // Get user's location on component mount
   useEffect(() => {
-    if ('geolocation' in navigator) {
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           setLocation({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
           });
-          console.log("User's Location:", position.coords);
+          sendLocationToServer(position.coords.latitude, position.coords.longitude);
         },
         (error) => {
           setError(error.message);
@@ -20,9 +22,27 @@ const GeolocationComponent = () => {
         }
       );
     } else {
-      setError('Geolocation is not available');
+      setError('Geolocation is not supported by this browser.');
     }
   }, []);
+
+  // Send location to server
+  const sendLocationToServer = async (latitude, longitude) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_SERVER_URI}/api/location/update`,
+        { latitude, longitude },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log('Location sent to server:', response.data);
+    } catch (error) {
+      console.error('Error sending location to server:', error);
+    }
+  };
 
   return (
     <div>
