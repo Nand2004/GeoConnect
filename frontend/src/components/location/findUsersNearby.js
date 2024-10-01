5c5c391a2321aad8995bf37107565a7f134cdbd5
Nand2004 <nandp480@gmail.com>
2024-10-01 11:57:10 -0400

    Add information for Google API markers

--- frontend/src/components/location/findUsersNearby.js
+++ frontend/src/components/location/findUsersNearby.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Card, Button, Container, Row, Col, Spinner, Alert } from 'react-bootstrap';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const FindUsersNearby = () => {
   const [location, setLocation] = useState({ latitude: null, longitude: null });
@@ -8,6 +9,9 @@ const FindUsersNearby = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [message, setMessage] = useState('');
+  const [selectedUser, setSelectedUser] = useState(null); // State for selected user
+
+  const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY; // Use API key from env
 
   // Get user's location
   const getUserLocation = () => {
@@ -61,7 +65,7 @@ const FindUsersNearby = () => {
     borderRadius: '15px',
     padding: '30px',
     boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
-    color: 'white',
+    color: 'black',
     marginTop: '30px',
   };
 
@@ -160,6 +164,59 @@ const FindUsersNearby = () => {
             </Col>
           ))}
         </Row>
+      )}
+
+      {/* Google Maps Section */}
+      {location.latitude && location.longitude && (
+        <div style={{ marginTop: '30px' }}>
+          <LoadScript googleMapsApiKey={googleMapsApiKey}>
+            <GoogleMap
+              center={{
+                lat: location.latitude,
+                lng: location.longitude,
+              }}
+              zoom={16}
+              mapContainerStyle={{ height: '400px', width: '100%' }}
+            >
+              {/* Marker for current logged-in user */}
+              <Marker
+                position={{ lat: location.latitude, lng: location.longitude }}
+                icon={{
+                  url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                }}
+              />
+
+              {/* Markers for nearby users */}
+              {nearbyUsers.map((user, index) => (
+                <Marker
+                  key={index}
+                  position={{ lat: user.location.coordinates[1], lng: user.location.coordinates[0] }}
+                  icon={{
+                    url: 'http://maps.google.com/mapfiles/ms/icons/pink-dot.png',
+                  }}
+                  onClick={() => setSelectedUser(user)} // Set the selected user on marker click
+                />
+              ))}
+
+              {/* InfoWindow for selected user */}
+              {selectedUser && (
+                <InfoWindow
+                  position={{
+                    lat: selectedUser.location.coordinates[1],
+                    lng: selectedUser.location.coordinates[0],
+                  }}
+                  onCloseClick={() => setSelectedUser(null)} // Close InfoWindow
+                >
+                  <div>
+                    <h3>{selectedUser.username}</h3>
+                    <p>Latitude: {selectedUser.location.coordinates[1]}</p>
+                    <p>Longitude: {selectedUser.location.coordinates[0]}</p>
+                  </div>
+                </InfoWindow>
+              )}
+            </GoogleMap>
+          </LoadScript>
+        </div>
       )}
     </Container>
   );
